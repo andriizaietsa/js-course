83275dc59a7b58069a0e1657c3e0612d6acd5597
zaiets.andrii <zaiets.andrii@globaldev.tech>
2023-12-15 09:23:45 +0200

    add toggleVisibleMyDB, validation for Genres

--- js/movie.js
+++ js/movie.js
@@ -1,90 +1,82 @@
-let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt("How many films did you watch?", "amount of films");
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("How many films did you watch?", "amount of films");
-    }
-}
-start();
-
-let i = 0;
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
-}
+    private: false,
+    start: function () {
+        personalMovieDB.count = +prompt("How many films did you watch?", "amount of films");
 
-function showMyDB(privateStatus) {
-    if (privateStatus == false) {
-        console.log(personalMovieDB)
-    }
-    else {
-        console.log("Private is not false")
-    }
-}
-
-showMyDB(personalMovieDB.private);
-
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`What genre do you like? its answer number ${i}`, "");
-        console.log(personalMovieDB);
-    }
-}
-writeYourGenres();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert("Watched not much films");
-    }
-    else if (numberOfFilms >= 10 && numberOfFilms < 30) {
-        alert("You are usual film watcher");
-    }
-    else if (numberOfFilms >= 30) {
-        alert("You are really like films");
-    }
-    else {
-        alert("There is a error")
-    }
-}
-
-detectPersonalLevel();
-
-
-function rememberMyFilms() {
-    while (i < numberOfFilms) {
-        i++;
-        const lastWatchedFilm = prompt(`What is your last watched film? (it is about ${i} film)`, "");
-        const whatIsYourRate = +prompt(`What is your rate for ${i} film?`, "")
-        //check field input
-        if (whatIsYourRate === 0) {
-            i--;
-            console.log("whatIsYourRate error");
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("How many films did you watch?", "amount of films");
         }
-        else if (lastWatchedFilm == null || lastWatchedFilm.length == 0 || lastWatchedFilm.length > 50) {
-            i--;
-            console.log("lastWatchedFilm.length")
+    },
+    rememberMyFilms: function () {
+        let i = 0;
+        while (i < personalMovieDB.count) {
+            i++;
+            const lastWatchedFilm = prompt(`What is your last watched film? (it is about ${i} film)`, "");
+            const whatIsYourRate = +prompt(`What is your rate for ${i} film?`, "")
+            //check field input
+            if (whatIsYourRate === 0) {
+                i--;
+                console.log("whatIsYourRate error");
+            }
+            else if (lastWatchedFilm == null || lastWatchedFilm.length == 0 || lastWatchedFilm.length > 50) {
+                i--;
+                console.log("lastWatchedFilm.length")
+            }
+            personalMovieDB.movies[lastWatchedFilm] = whatIsYourRate;
         }
-        personalMovieDB.movies[lastWatchedFilm] = whatIsYourRate;
-        console.log(personalMovieDB.movies);
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            alert("Watched not much films");
+        }
+        else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+            alert("You are usual film watcher");
+        }
+        else if (numberOfFilms >= 30) {
+            alert("You are really like films");
+        }
+        else {
+            alert("There is a error")
+        }
+    },
+    showMyDB: function () {
+        if (personalMovieDB.private == false) {
+            console.log(personalMovieDB)
+        }
+        else {
+            console.log("Private is not false")
+        }
+    },
+    writeYourGenres: function () {
+        let k = 0;
+        while (k <= 2) {
+            k++;
+            personalMovieDB.genres[k - 1] = prompt(`What genre do you like? its answer number ${k}`, "");
+
+            if (personalMovieDB.genres == '' || personalMovieDB.genres == null) {
+                k--;
+            }
+        }
+    
+        // for (const [key, value] of personalMovieDB.genres){
+        //     console.log(`${key}: ${value}`);
+        // }
+
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.private == false) {
+            personalMovieDB.private = true
+        }
+        else if (personalMovieDB.private == true) {
+            personalMovieDB.private = false
+        }
     }
 }
 
-rememberMyFilms();
-
-
-/* Задание на урок:
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки, отменить ответ или
- ввести название фильма длинее, чем 50 символов. Если это происходит возвращаем пользователя к вопросам опять.
-  (К любой строке можно обратиться как str. length - и получить её длину)
-
-
-3) При помощи условий проверить personalMovieDB. count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-4) Потренироваться и переписать цикл еще двумя способами*/
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
--- jsPractice.js
+++ jsPractice.js
@@ -1,15 +1,82 @@
-function getMathResult(baseNumber, repeatNumber) {
-    if (typeof repeatNumber === "string" || repeatNumber <= 0) {
-        return baseNumber;
+//1st
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    if (arr.length > 1) {
+        return "Семья состоит из: " + arr.join(" ");
     }
-    let resultArr = [];
-    for (let i = 1; i <= repeatNumber; i++) {
-        resultArr.push(baseNumber * i);
+    else{
+        return 'Семья пуста';
     }
-    console.log(resultArr.join(' --- '));
+
 }
+showFamily(family);
 
-getMathResult(5, 3);
+//2nd
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(function(item, i, arr){
+        return `${item.toLowerCase()}`
+    })
+}
+standardizeStrings(favoriteCities)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+//     showAgeAndLangs: function(){
+//         let age = personalPlanPeter.age;
+//         let result = "";
+//         for(let key in personalPlanPeter.skills.languages){
+//             result += personalPlanPeter.skills.languages[key] + " ";
+//         }
+//         let upperCaseResult = result.toLocaleUpperCase();
+//         return `Мне ${age} и я владею языками: ${upperCaseResult}`
+//     }
+// };
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+
+// function showProgrammingLangs(plan) {
+//     const arrObjects = Object.keys(plan.skills.programmingLangs)
+//     const number = arrObjects.length;
+//     if (number <= 0) {
+//         //return ""
+//         console.log("")
+//     }
+//     else {
+//         for (let key in plan.skills.programmingLangs) {
+//             let lang = key
+//             let percent = personalPlanPeter.skills.programmingLangs[key];
+//             //return `Язык ${lang} изучен на ${percent}`
+//             console.log(`Язык ${lang} изучен на ${percent}`);
+//         }
+//     }
+
+// }
+// showProgrammingLangs(personalPlanPeter)
