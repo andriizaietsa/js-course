e68aa46acd2bea8f42bdf4caa5f6307301312788
zaiets.andrii <zaiets.andrii@globaldev.tech>
2023-12-22 11:36:27 +0200

    students solved

--- jsPractice.js
+++ jsPractice.js
@@ -1,22 +1,25 @@
 const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
 function sortStudentsByGroups(arr) {
-    //sorting
     const sorting = arr.sort();
 
-    //split out to three teams
     let teamArr = [];
     let temp = [];
     for (let i = 0; i < 9; i += 3) {
         temp = sorting.slice(i, i + 3);
         teamArr.push(temp);
     }
-    //get extra students 
+
     let getExtraStudents = sorting.slice(9, arr.length);
-    let amountExtraStudents = getExtraStudents.toString();
-    console.log(amountExtraStudents);
-    teamArr.push('Оставшиеся студенты: ' + amountExtraStudents);
-    console.log(teamArr);
+    let listOfExtraStudents = getExtraStudents.join(', ');
+    let amountOfExtraStudents = arr.length - 9
+    if(amountOfExtraStudents > 0){
+        teamArr.push('Оставшиеся студенты: ' + listOfExtraStudents);
+    }
+    else{
+        teamArr.push('Оставшиеся студенты: -');
+    }
+    return teamArr;
 }
 
 sortStudentsByGroups(students);
